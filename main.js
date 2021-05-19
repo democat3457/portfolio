547523343636699cb7a97123a69b6ff4aecd1ede
Colin Wong <cwongmath@gmail.com>
2021-05-19 11:52:42 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ console.log("hi");
 $(function(){
   Array.from(document.querySelectorAll("ul.topnav a.text"))
        .filter(link => 
-            ((window.location.pathname.endsWith("portfolio") || window.location.pathname.endsWith("portfolio/")) && 
+            ((window.location.pathname.endsWith("portfolio") || window.location.pathname.endsWith("portfolio/") || window.location.pathname.endsWith("index.html")) && 
               (link.href.endsWith("portfolio") || link.href.endsWith("portfolio/") || link.href.indexOf("index.html") !== -1)) || 
             (!(window.location.pathname.endsWith("portfolio") || window.location.pathname.endsWith("portfolio/")) && 
               link.href.indexOf(window.location.pathname) !== -1))
