5e6a37b183d393e7511da409ba19694650544785
Colin Wong <cwongmath@gmail.com>
2021-05-21 18:46:59 -0500

    Update import.js

--- import.js
+++ import.js
@@ -1,9 +1,9 @@
 $(function(){
-  $("head").load("parts/head.html");
+  $("head").load("/portfolio/parts/head.html");
   $.get("parts/nav.html", function(data) {
     $("#temp-nav").replaceWith(data);
   });
-  $.get("parts/footer.html", function(data) {
+  $.get("/portfolio/parts/footer.html", function(data) {
     $("#temp-footer").replaceWith(data);
   });
 });
